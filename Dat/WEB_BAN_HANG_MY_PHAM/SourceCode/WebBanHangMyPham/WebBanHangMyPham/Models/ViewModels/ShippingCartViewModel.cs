65bad05c2928e76efb104fbb13c07387a1a976a1
AlvinDMartin <AlvinDMartin1999@users.noreply.github.com>
2019-12-10 20:55:13 +0700

    updateMaster1

---
+++ Dat/WEB_BAN_HANG_MY_PHAM/SourceCode/WebBanHangMyPham/WebBanHangMyPham/Models/ViewModels/ShippingCartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBanHangMyPham.Models.ViewModels
+{
+    public class ShippingCartViewModel
+    {
+        public List<ThongTinSanPham> ThongTinSanPhams { get; set; }
+        public Appointments Appointments { get; set; }
+    }
+}
